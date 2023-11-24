cb2694d7167f03e4abb34edb5c85f830c1fe50eb
Anthony Peng <anthonyhug222@gmail.com>
2023-11-23 19:08:16 -0500

    initial react app setup

---
+++ presidendle/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {useState, useEffect} from "react"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Presidendle</h1>
+      <h3>Try To Guess The Country Where The President Is From!</h3>
+    </div>
+  );
+}
+
+export default App;
