2b632ea2a9b1c2fee6a0a069f45ec1ed189f1606
Anthony Peng <anthonyhug222@gmail.com>
2023-12-22 16:44:18 -0500

    added picture and dropdown list for searching president's name

--- presidendle/src/App.js
+++ presidendle/src/App.js
@@ -1,13 +1,74 @@
 import './App.css';
-import {useState, useEffect} from "react"
+import {useState, useEffect, useCallback} from "react"
+import Select from 'react-select';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Presidendle</h1>
-      <h3>Try To Guess The Country Where The President Is From!</h3>
-    </div>
-  );
-}
+  const [presidents, setPresidents] = useState([]);
+  const [selectedPresident, setSelectedPresident] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const options = presidents.map((president) => ({
+    value: president.id,
+    label: president.name,
+  }));
+  const defaultOption = {value: '', label: 'Type or select a president\'s name'};
+    
+  const handleOptionChange = (selectedOption) => {
+    console.log(selectedOption)
+  };
+ 
+  const fetchData = useCallback(async () => {
+  try {
+    setIsLoading(true); // Set loading state to true at the start of fetch
+    const response = await fetch('https://api.sampleapis.com/presidents/presidents');
+    const presidentsArray = await response.json();
+
+    setPresidents(presidentsArray);
+    setSelectedPresident(getRandomPresident(presidentsArray));
+
+    setIsLoading(false);
+  } catch (error) {
+    console.log('**Fetch exception:', error);
+
+    setIsLoading(false);
+  }
+  }, []);
+ 
+  const getRandomPresident = (presidentsArray) => {
+  const randomIndex = Math.floor(Math.random() * presidentsArray.length);
+  return presidentsArray[randomIndex];
+  };
+ 
+  useEffect(() => {
+  fetchData();
+  }, [fetchData]);
+
+return (
+  <div className="App">
+    <h1>Presidendle</h1>
+    <h3>Try To Guess Who The President Is!</h3>
+    {isLoading ? null : selectedPresident ? (
+      <>
+        <div className='image'>
+          <img src={selectedPresident.photo} alt="" onLoad={() => setIsImageLoaded(true)}/>
+        </div>
+        {isImageLoaded && (
+          <div className='text-field'>
+            <Select
+              options={options}
+              value={defaultOption}
+              onChange={handleOptionChange}
+              isSearchable={true}
+              className='select'
+            />
+          </div>
+        )}
+      </>
+    ) : (
+      <p>Unable to Access Data ERROR</p>
+    )}
+  </div>
+);
+ }
 
 export default App;
