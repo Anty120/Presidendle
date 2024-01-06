9ae2059a66401eb2a337a694e724a8929d51502c
Anthony Peng <anthonyhug222@gmail.com>
2024-01-05 23:48:35 -0500

    changed feedback system to use arrows to show higher or lower rather than using common letters

--- presidendle/src/App.js
+++ presidendle/src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect, useCallback } from "react"
 import Select from 'react-select';
 import { findPresident } from 'us-presidents';
 import ReactModal from 'react-modal'
+import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
 
 function App() {
     const [presidents, setPresidents] = useState([]);
@@ -29,54 +30,44 @@ function App() {
     };
     const handleOptionChange = (selectedOption) => {
         if (guesses === 0) {
-            console.log("No guesses left")
+            console.log("SHOULD TRIGGER LOSE MODAL NOW")
             return;
         }
 
         setGuesses(guesses - 1);
 
         if (selectedOption.value === selectedPresident.id) {
-            console.log("You guessed the president's name")
             openModal()
         } else {
-            const commonLetters = getCommonLetters(selectedOption.label, selectedPresident.name);
-            console.log(`Oops! That guess is incorrect. You guessed ${commonLetters} common letter(s) though. Keep trying!`);
+            console.log(selectedOption)
+            const arrow = selectedOption.value < selectedPresident.id ? <FaArrowDown /> : <FaArrowUp />;
+            setSelectedOptions([...selectedOptions,
+            { label: selectedOption.label, arrow: arrow, value: selectedOption.value }
+            ]);
         }
-
-        setSelectedOptions([...selectedOptions,
-        { label: selectedOption.label, commonLetters: getCommonLetters(selectedOption.label, selectedPresident.name), value: selectedOption.value }
-        ]);
     };
-
-    function getCommonLetters(guess, correctName) { //but in an unsorted manner
-        const guessLetters = guess.toLowerCase().split('');
-        const correctLetters = correctName.toLowerCase().split('');
-        const commonLettersSet = new Set(guessLetters.filter(letter => correctLetters.includes(letter) && letter !== ' '));
-        const commonLetters = Array.from(commonLettersSet);
-        commonLetters.sort(() => Math.random() - 0.5);
-        return commonLetters;
-    }
 
     const fetchData = useCallback(async () => {
         try {
             setIsLoading(true); // Set loading state to true at the start of fetch
             const response = await fetch('https://api.sampleapis.com/presidents/presidents');
             const presidentsArray = await response.json();
-            presidentsArray.filter((_, index) => index !== 2); //cannot have two because the link for the picture is broken
-
-            setPresidents(presidentsArray);
-            setSelectedPresident(getRandomPresident(presidentsArray));
+            
+            const filteredPresidentsArray = presidentsArray.filter((_, index) => index !== 1); //cannot have two because the link for the picture is broken and not 22 because there is no description for Chester A. Arthur
+            console.log(filteredPresidentsArray)
+            setPresidents(filteredPresidentsArray);
+            setSelectedPresident(getRandomPresident(filteredPresidentsArray));
 
             setIsLoading(false);
         } catch (error) {
             console.log('**Fetch exception:', error);
-
             setIsLoading(false);
         }
     }, []);
 
     const getRandomPresident = (presidentsArray) => {
         const randomIndex = Math.floor(Math.random() * presidentsArray.length);
+        console.log(presidentsArray[randomIndex])
         return presidentsArray[randomIndex];
     };
 
@@ -112,7 +103,7 @@ function App() {
                                             <div className='vertical-line'></div>
                                             <span>{option.label}</span>
                                             <div className='vertical-line' />
-                                            <span>Common letters: {option.commonLetters.join(' ')}</span>
+                                            {option.arrow}
                                         </div>
                                     ))}
                                 </div>
